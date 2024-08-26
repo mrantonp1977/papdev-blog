562f424a6866cb4c96bff2443c64c0895056a4e6
Antonis Papaioannou <mrantonp@gmail.com>
2024-08-27 00:35:27 +0300

    feat: Add Badge component for displaying badges

--- app/dashboard/sites/[siteId]/page.tsx
+++ app/dashboard/sites/[siteId]/page.tsx
@@ -1,10 +1,30 @@
 import prisma from '@/app/utils/db';
 import { Button } from '@/components/ui/button';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { Book, FileIcon, PlusCircle, Settings } from 'lucide-react';
+import { Book, FileIcon, MoreHorizontal, PlusCircle, Settings } from 'lucide-react';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Badge } from '@/components/ui/badge';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Image from 'next/image';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 
 async function getData(userId: string, siteId: string) {
   const data = await prisma.post.findMany({
@@ -76,7 +96,75 @@ const SiteIdRoute = async ({ params }: { params: { siteId: string } }) => {
           </Button>
         </div>
       ) : (
-        <h1>Data</h1>
+        <div className="">
+          <Card>
+            <CardHeader>
+              <CardTitle>Articles</CardTitle>
+              <CardDescription>
+                Manage your articles in a simple and intuitive interface.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Image</TableHead>
+                    <TableHead>Title</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Created At</TableHead>
+                    <TableHead className="text-right">Actions</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {data.map((item) => (
+                    <TableRow key={item.id}>
+                      <TableCell>
+                        <Image
+                          src={item.image}
+                          alt={item.title}
+                          width={64}
+                          height={64}
+                          className="size-16 rounded-md object-cover"
+                        />
+                      </TableCell>
+                      <TableCell className="font-medium">
+                        {item.title}
+                      </TableCell>
+                      <TableCell>
+                        <Badge
+                          className="bg-green-500/10 text-green-500"
+                          variant="outline"
+                        >
+                          Published
+                        </Badge>
+                      </TableCell>
+                      <TableCell>
+                        {new Date(item.createdAt).toLocaleDateString()}
+                      </TableCell>
+                      <TableCell className="text-end">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button size="icon" variant="ghost">
+                              <MoreHorizontal className="size-4"/>
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align='center'>
+                            <DropdownMenuLabel>
+                              Actions
+                            </DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                            <DropdownMenuItem className="text-red-500">Delete</DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </div>
       )}
     </>
   );
